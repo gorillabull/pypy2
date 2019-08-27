c58aea6dd626bbe9780b7406e4c244133aaf8e9f
Leonardo  Vasillev <vasilevl@ad.cs.unlv.edu>
2019-08-27 14:17:45 -0700

    sdf

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -5,73 +5,39 @@ visited = [False for x in range(1000)]
 adj = [[0 for x in range(10)]for y in range(10)]    
 adj2 = [[]] # [[0 for x in range(100)]for y in range(100)]
 
-#this is how they are given as input 
-adj[1][2]=1
-adj[3][1]=1
-adj[2][4]=1
-adj[1][2]=1
-adj[2][3]=1
-adj[5][6]=1
+#this is how they are given as input
+adj[1][2] = 1
+adj[3][1] = 1
+adj[2][4] = 1
+adj[1][2] = 1
+adj[2][3] = 1
+adj[5][6] = 1
 
-#number of nodes in the array 
-nodes = 6
 
-#number of connected components in the entire graph 
-conComp = 0 ;
-
-#count of subtrees 
-ct=0 
 
 
 def dfs(s):
-    visited[s]=True;
+    visited[s] = True
     for x in range(0,adj2[s].__len__(),1):
-        if(visited[adj2[s][x]]==False):
-            p=adj2[s][x]
+        if(visited[adj2[s][x]] == False):
+            p = adj2[s][x]
             print(p)
             dfs(adj2[s][x])
             
 
 
 
-
 def hr1(a,b,c,d):
-    #append the nodes into the list 
+    #append the nodes into the list
     for x in range(0,d.__len__(),1):
-        for y in range(0,d[x].__len__(),1):
-            if d[x][y] ==1:
-                adj2.append([]);
-                adj2[x].append(y)
-
-#append a bunch of extra nodes for missing elements 
-for x in range(100  ):
-    adj2.append([0])
+        adj2.append([])
+        adj2[d[x][0]].append(d[x][1])
 
 
-
-#init the adjacency lists
-hr1(1,1,1,adj)
-
-for x in range(1,nodes + 1,  1 ):
-    if visited[x] ==False:
-        dfs(x);
-        conComp = conComp+1; 
-
-print("---------------")
-print(conComp)
-
-
-dfs(6)
-
-
-#final calculation, if more than one subtree 
-#subtree count * lib_cost (at least one library in each subtree is required) + (number of nodes - Subtree count )
-
-
-if __name__ == '__main__':
-
+def main():
     q = int(input())
 
+    #get the input stuff
     for q_itr in range(q):
         nmC_libC_road = input().split()
 
@@ -88,9 +54,56 @@ if __name__ == '__main__':
         for _ in range(m):
             cities.append(list(map(int, input().rstrip().split())))
 
-       # result = roadsAndLibraries(n, c_lib, c_road, cities)
-
-        print(result)
+        shiiet(n,c_lib, c_road, cities)
 
 
+def shiiet(n, c_lib, c_road, d):
+        #number of nodes in the array
+    nodes = 6
+
+    #number of connected components in the entire graph
+    conComp = 0
+
+    #count of subtrees
+    ct = 0 
+
+    #return value
+    result = 0
+    #append a bunch of extra nodes for missing elements
+    for x in range(1000000):
+        adj2.append([0])
+
+    nodes = n
+ 
+    #init adjacency lists
+    hr1(1,1,1,d)
+
+    for x in range(1,nodes + 1,  1):
+        if visited[x] == False:
+            dfs(x)
+            conComp = conComp + 1 
+
+    print("---------------")
+    print(conComp)
+
+    result = conComp * c_lib
+    cheapest = 0
+
+    if c_lib <= c_road:
+        cheapest = c_lib
+    else:
+        cheapest = c_road
+
+    result = result + (n - conComp) * cheapest 
+
+    #print(result)
+
+    return result
+    
+    
+
+
+main()
+
+s = 5 
 
